23b6cfab7b6d115116ab814ef58139aa0284675c
ptaylor <paul.e.taylor@me.com>
2018-07-09 14:21:17 -0700

    guard against reading junk messageLength

--- js/src/ipc/reader/node.ts
+++ js/src/ipc/reader/node.ts
@@ -54,7 +54,7 @@ export async function* fromReadableStream(stream: NodeJS.ReadableStream) {
             messageLength = new DataView(bytes.buffer).getInt32(0, true);
         }
 
-        while (messageLength < bytes.byteLength) {
+        while (messageLength > 0 && messageLength < bytes.byteLength) {
             if (!message) {
                 (bb = new ByteBuffer(bytes)).setPosition(4);
                 if (message = _Message.getRootAsMessage(bb)) {
