983db564ce50a2ae9e423bab7911111d95d57447
ptaylor <paul.e.taylor@me.com>
2018-11-19 15:45:53 -0600

    autobind some visitor prototype method implementations (perf)

--- js/src/visitor/iterator.ts
+++ js/src/visitor/iterator.ts
@@ -125,3 +125,47 @@ function vectorIterator<T extends DataType>(vector: Vector<T>): IterableIterator
         }
     })(getVisitor.getVisitFn(vector));
 }
+
+IteratorVisitor.prototype.visitNull                 = vectorIterator;
+IteratorVisitor.prototype.visitBool                 = vectorIterator;
+IteratorVisitor.prototype.visitInt                  = vectorIterator;
+IteratorVisitor.prototype.visitInt8                 = vectorIterator;
+IteratorVisitor.prototype.visitInt16                = vectorIterator;
+IteratorVisitor.prototype.visitInt32                = vectorIterator;
+IteratorVisitor.prototype.visitInt64                = vectorIterator;
+IteratorVisitor.prototype.visitUint8                = vectorIterator;
+IteratorVisitor.prototype.visitUint16               = vectorIterator;
+IteratorVisitor.prototype.visitUint32               = vectorIterator;
+IteratorVisitor.prototype.visitUint64               = vectorIterator;
+IteratorVisitor.prototype.visitFloat                = vectorIterator;
+IteratorVisitor.prototype.visitFloat16              = vectorIterator;
+IteratorVisitor.prototype.visitFloat32              = vectorIterator;
+IteratorVisitor.prototype.visitFloat64              = vectorIterator;
+IteratorVisitor.prototype.visitUtf8                 = vectorIterator;
+IteratorVisitor.prototype.visitBinary               = vectorIterator;
+IteratorVisitor.prototype.visitFixedSizeBinary      = vectorIterator;
+IteratorVisitor.prototype.visitDate                 = vectorIterator;
+IteratorVisitor.prototype.visitDateDay              = vectorIterator;
+IteratorVisitor.prototype.visitDateMillisecond      = vectorIterator;
+IteratorVisitor.prototype.visitTimestamp            = vectorIterator;
+IteratorVisitor.prototype.visitTimestampSecond      = vectorIterator;
+IteratorVisitor.prototype.visitTimestampMillisecond = vectorIterator;
+IteratorVisitor.prototype.visitTimestampMicrosecond = vectorIterator;
+IteratorVisitor.prototype.visitTimestampNanosecond  = vectorIterator;
+IteratorVisitor.prototype.visitTime                 = vectorIterator;
+IteratorVisitor.prototype.visitTimeSecond           = vectorIterator;
+IteratorVisitor.prototype.visitTimeMillisecond      = vectorIterator;
+IteratorVisitor.prototype.visitTimeMicrosecond      = vectorIterator;
+IteratorVisitor.prototype.visitTimeNanosecond       = vectorIterator;
+IteratorVisitor.prototype.visitDecimal              = vectorIterator;
+IteratorVisitor.prototype.visitList                 = vectorIterator;
+IteratorVisitor.prototype.visitStruct               = vectorIterator;
+IteratorVisitor.prototype.visitUnion                = vectorIterator;
+IteratorVisitor.prototype.visitDenseUnion           = vectorIterator;
+IteratorVisitor.prototype.visitSparseUnion          = vectorIterator;
+IteratorVisitor.prototype.visitDictionary           = vectorIterator;
+IteratorVisitor.prototype.visitInterval             = vectorIterator;
+IteratorVisitor.prototype.visitIntervalDayTime      = vectorIterator;
+IteratorVisitor.prototype.visitIntervalYearMonth    = vectorIterator;
+IteratorVisitor.prototype.visitFixedSizeList        = vectorIterator;
+IteratorVisitor.prototype.visitMap                  = vectorIterator;
